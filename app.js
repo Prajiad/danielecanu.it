3300439bdcc0214b0ad5c9bdb6c2ba8eab8e9c99
Daniele <42892431+Prajiad@users.noreply.github.com>
2022-06-21 17:01:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    autoStart: true,
+    loop: false,
+    delay: '100',
+    cursor: '',
+});
+
+typewriter
+    .typeString('Prajiad')
+    .pauseFor(1000)
+    .start();
+
+var app = document.getElementById('app2');
+
+var typewriter = new Typewriter(app, {
+    autoStart: true,
+    loop: false,
+    delay: 'natural',
+    cursor: '|',
+});
+
+typewriter
+    .typeString('./prajiad/about_me/A random guy, gamer and coder (maybe).')
+    .pauseFor(1000)
+    .start();
+
+    var app = document.getElementById('app3');
+
+    var typewriter = new Typewriter(app, {
+        autoStart: true,
+        loop: false,
+        delay: 'natural',
+        cursor: '',
+    });
+    
+    typewriter
+        .typeString('Not responsive yet :/')
+        .pauseFor(1000)
+        .start();
